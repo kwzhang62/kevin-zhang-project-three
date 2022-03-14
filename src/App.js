f728918b25eecc8f911b3a34ab1f4a1585a142e8
KevZ <95584217+kwzhang62@users.noreply.github.com>
2022-03-14 01:30:18 -0400

    added firebase connection and data loading on initial page render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase';
+//import react hooks
+import { useEffect, useState } from 'react';
+//import firebase modules
+import { getDatabase, ref, onValue } from 'firebase/database';
 
 function App() {
+
+  //set and update the commit message to be displayed using state
+  const [commitMessage, setCommitMessage] = useState("");
+
+  //load the messages from the database on initial page render
+  useEffect( () => {
+    const database = getDatabase(firebase);
+    const dbRef = ref(database);
+    onValue(dbRef, (response) => {
+      //parse the response from the db
+      const data = response.val();
+      
+      //the messages are stored in the db as a single string with each message separated by semi-colons
+      //parse the string into an array
+      const commitMessagesArray = data.commitMessages.split(";");
+      
+      //choose a random message from the array and update the commitMessage state
+      const index = randomIndex(commitMessagesArray.length);
+      const randomMessage = commitMessagesArray[index];
+      setCommitMessage(randomMessage);
+    });
+  }, []);
+
+  //return a random index number for an array
+  function randomIndex(arrayLength) {
+    return Math.floor(Math.random() * arrayLength);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Take a commit, Leave a commit</h1>
       </header>
-    </div>
+      <main>
+        <p>check the console</p>
+      </main>
+    </>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALFMufE2kRNncVk5WsQotQANOQ3xqYIP0",
+  authDomain: "project-three-tclc.firebaseapp.com",
+  projectId: "project-three-tclc",
+  storageBucket: "project-three-tclc.appspot.com",
+  messagingSenderId: "993680807987",
+  appId: "1:993680807987:web:f220e8cba2f66789466b46"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
